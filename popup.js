e3644e3aaf438cec656d4211637ab3939ab111e7
Wesley <nickeley101@gmail.com>
2021-08-06 19:29:47 -0400

    made slow mode go

--- popup.js
+++ popup.js
@@ -1,17 +1,17 @@
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("solveMine");
+let sweepMines = document.getElementById("solveMine");
 
 chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+  sweepMines.style.backgroundColor = color;
 });
 
 // When the button is clicked, inject simulateClick into current page
-changeColor.addEventListener("click", async () => {
+sweepMines.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     //function: simulateClick,
-    files: ['script.js'],
+    files: ['test.js'],
   });
 });
--- script.js
+++ script.js
@@ -1,94 +1,94 @@
-
-
-function loop(leftMouseDownEvent, leftMouseUpEvent, rightMouseDownEvent, rightMouseUpEvent) {
-  console.log("loop");
-
+function myLoop(leftMouseDownEvent, leftMouseUpEvent, rightMouseDownEvent, rightMouseUpEvent) {         //  create a loop function
   face = document.getElementById("face");
   height = 16;
   width = 16;
   blanks = 0;
   flags = 0;
   hopeless = 0; 
-  while (face.className !== 'facewin' && hopeless < 3) {
+  setTimeout(function() {   //  call a 3s setTimeout when the loop is called
 
-    cycle = true;
-    for (let i = 1; i <= height && cycle; i++) { //iterate through each square until a change is made to the board
-      for (let j = 1; j <= width && cycle; j++) {
+      cycle = true;
+      for (let i = 1; i <= height && cycle; i++) { //iterate through each square until a change is made to the board
+        for (let j = 1; j <= width && cycle; j++) {
+  
+          squareID = document.getElementById(i + "_" + j);
 
-        squareID = document.getElementById(i + "_" + j);
-
-        if (squareID.className.startsWith('square open') && squareID.className !== 'square open0') {
-          var squareNum = squareID.className[squareID.className.length - 1];
-          blanks = 0; //reset the blank count for each square
-          flags = 0; //reset the flags count for each square
-          for (let m = -1; m <= 1; m++){ //count how many blank/flags are around the square
-            for (let n = -1; n <= 1; n++) {
-              x = m + i;
-              y = n + j;
-              surr = document.getElementById(x + "_" + y).className;
-              if (y!= 0 && y <= width && x != 0 && x <= height && surr === 'square blank') {
-                blanks++;
+          if (squareID.className.startsWith('square open') && squareID.className !== 'square open0') {
+            var squareNum = squareID.className[squareID.className.length - 1];
+            blanks = 0; //reset the blank count for each square
+            flags = 0; //reset the flags count for each square
+            for (let m = -1; m <= 1; m++){ //count how many blank/flags are around the square
+              for (let n = -1; n <= 1; n++) {
+                x = m + i;
+                y = n + j;
+                surr = document.getElementById(x + "_" + y).className;
+                if (y!= 0 && y <= width && x != 0 && x <= height && surr === 'square blank') {
+                  blanks++;
+                }
+                else if (y!= 0 && y <= width && x != 0 && x <= height && surr === 'square bombflagged') {
+                  flags++;
+                }
+  
               }
-              else if (y!= 0 && y <= width && x != 0 && x <= height && surr === 'square bombflagged') {
-                flags++;
-              }
-
             }
-          }
+  
+  
+            if (blanks == 0) {//lol idk how to short circuit in js
+  
+            } else if (squareNum == flags) { //if the squareNum is equal to the flags, all blank squares surrounding are not bombs
+              cycle = false;  
+              hopeless = 0;            
+              for (let m = -1; m <= 1; m++){ //click surrounding squares
+                for (let n = -1; n <= 1; n++) {
+                  x = m + i;
+                  y = n + j;
+                  surr = document.getElementById(x + "_" + y);
+                  if (surr.className === 'square blank') { 
+                    if (y!= 0 && y <= width && x != 0 && x <= height){ //don't change the out of bounds squares
 
-
-          if (blanks == 0) {//lol idk how to short circuit in js
-
-          } else if (squareNum == flags) { //if the squareNum is equal to the flags, all blank squares surrounding are not bombs
-            cycle = false;  
-            hopeless = 0;            
-            for (let m = -1; m <= 1; m++){ //click surrounding squares
-              for (let n = -1; n <= 1; n++) {
-                x = m + i;
-                y = n + j;
-                surr = document.getElementById(x + "_" + y);
-                if (surr.className === 'square blank') { 
-                  if (y!= 0 && y <= width && x != 0 && x <= height){ //don't change the out of bounds squares
-                    surr.dispatchEvent(leftMouseDownEvent);
-                    surr.dispatchEvent(leftMouseUpEvent);    
+                      surr.dispatchEvent(leftMouseDownEvent);
+                      surr.dispatchEvent(leftMouseUpEvent);    
+                    }
                   }
                 }
-              }
-            }  
-          } 
-          else if (blanks + flags == squareNum) { //if the squareNum is equal to the num of blanks + flags, all surrounding squares are flags
-            cycle = false;    
-            hopeless = 0;             
-            for (let m = -1; m <= 1; m++){ //flag surrounding squares
-              for (let n = -1; n <= 1; n++) {
-
-                x = m + i;
-                y = n + j;
-                surr = document.getElementById(x + "_" + y);
-                if (surr.className === 'square blank') { 
-                  if (y!= 0 && y <= width && x != 0 && x <= height){//don't change the out of bounds squares
-
-                    surr.dispatchEvent(rightMouseDownEvent);
-                    surr.dispatchEvent(rightMouseUpEvent);  
+              }  
+            } 
+            else if (blanks + flags == squareNum) { //if the squareNum is equal to the num of blanks + flags, all surrounding squares are flags
+              cycle = false;    
+              hopeless = 0;             
+              for (let m = -1; m <= 1; m++){ //flag surrounding squares
+                for (let n = -1; n <= 1; n++) {
+  
+                  x = m + i;
+                  y = n + j;
+                  surr = document.getElementById(x + "_" + y);
+                  if (surr.className === 'square blank') { 
+                    if (y!= 0 && y <= width && x != 0 && x <= height){//don't change the out of bounds squares
+                      surr.dispatchEvent(rightMouseDownEvent);
+                      surr.dispatchEvent(rightMouseUpEvent);  
+                    }
                   }
                 }
-              }
-            }            
-          }
-          else {
-            hopeless++;
+              }            
+            }
+            else {
+              hopeless++;
+            }
           }
         }
       }
-    }
-    face = document.getElementById("face");
-  }
+      face = document.getElementById("face");
+
+
+      if (face.className !== 'facewin' && hopeless < 3) {           //  if the counter < 10, call the loop function
+          myLoop(leftMouseDownEvent, leftMouseUpEvent, rightMouseDownEvent, rightMouseUpEvent);             //  ..  again which will trigger another 
+      }                       //  ..  setTimeout()
+  }, 200, leftMouseDownEvent, leftMouseUpEvent, rightMouseDownEvent, rightMouseUpEvent);
 }
 
 function simulateClick() {
-  console.log("in simulate click");
   document.title = 'working';
-  startingTile = "8_8"
+  startingTile = "4_5"
   squareID = document.getElementById(startingTile);
 
   const leftMouseDownEvent = new MouseEvent('mousedown', {
@@ -116,7 +116,7 @@ function simulateClick() {
   squareID.dispatchEvent(leftMouseDownEvent);
   squareID.dispatchEvent(leftMouseUpEvent);
 
-  setTimeout(loop, 10000, leftMouseDownEvent, leftMouseUpEvent, rightMouseDownEvent, rightMouseUpEvent);
+  myLoop(leftMouseDownEvent, leftMouseUpEvent, rightMouseDownEvent, rightMouseUpEvent);
 }
 
-simulateClick();
+simulateClick();                   //  start the loop
