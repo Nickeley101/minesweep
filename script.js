9b8c7bb65ebf3e1b49bb59957cd0eb78d8c2107d
Wesley <nickeley101@gmail.com>
2021-08-08 01:32:40 -0400

    got tank algorithm to work

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function myLoop(myMouseEvent) {         //  create a loop function
         }
 
         if (hopeless) {
-            tank(myMouseEvent, delay, dims);
+            hopeless = tank(myMouseEvent, delay, dims);
         }
 
         face = document.getElementById("face");
@@ -165,11 +165,140 @@ function tank(myMouseEvent, delay, dims) {
         }
     }
 
-    console.log(bigMatrix);
-    console.log(flagTot);
+    solve(bigMatrix, flagTot);
+    let thingsToClick = analyzeRow(bigMatrix); //things to click has an array of cleared cells and an array of flagged cells
 
+    if (thingsToClick[0].length + thingsToClick[1].length == 0) {
+        return true; //sets hopeless as true and stops
+    }
 
+    for (let i = 0; i < thingsToClick[0].length; i++) { //go through and click all cleared cells
+        let clearClick = document.getElementById(edgeSquares[thingsToClick[0][i]]);
 
+        clearClick.dispatchEvent(myMouseEvent[0]);
+        clearClick.dispatchEvent(myMouseEvent[1]);
+    }
+
+    for (let i = 0; i < thingsToClick[1].length; i++) { //go through and click all flag cells
+        let flagClick = document.getElementById(edgeSquares[thingsToClick[1][i]]);
+        flagClick.dispatchEvent(myMouseEvent[2]);
+        flagClick.dispatchEvent(myMouseEvent[3]);
+    }
+
+    return false;
+
+}
+//functions to figure out hard positions in tank algorithm
+function diagonalize(M) {
+    let m = M.length;
+    let n = M[0].length;
+    let nRow = 0;
+    for (let col = 0; col < n && nRow < m; ++col) {
+        // Find the col-th pivot
+        i_max = findPivot(M, col, nRow);//i_max is the row index with the pivot for the column
+        if (M[i_max][col] < 0) { //if the pivot is -1, make it positive
+            for(let j = 0; j < n; j++) {
+                M[i_max][j] = -M[i_max][j];
+            }
+        }
+
+        swap_rows(M, nRow, i_max);
+
+        if (M[nRow][col] != 0) {//if the column is all 0, don't increase row counter or do elimination
+
+            // eliminate column
+            for (let i = 0; i < m; ++i) {
+                if (nRow != i) {
+                    let c = M[i][col] / M[nRow][col];
+                    for (let j = col; j < n; ++j) {
+                        M[i][j] = M[i][j] - M[nRow][j] * c;
+                    }
+                }
+
+            }
+
+            nRow++;
+        }
+    }
+}
+
+function analyzeRow(M) {
+    let cleared = [];
+    let flag = [];
+    for (let i = 0; i < M.length; i++) {
+        let maxB = 0;
+        let minB = 0;
+        for (let j = 0; j < M[i].length - 1; j++) { //go through each element except for augmented column
+            if (M[i][j] == -1) {
+                minB--;
+            }
+            else if (M[i][j] == 1) {
+                maxB++;
+            }
+        }
+
+        if (maxB == M[i][M[i].length - 1]) { //if max bound equal to augmented number, postive values mines and negative values clear
+            for (let k = 0; k < M[i].length - 1; k++) {
+                if (M[i][k] == -1 && cleared.indexOf(k) === -1) {
+                    cleared.push(k);
+                }
+                else if (M[i][k] == 1 && flag.indexOf(k) === -1) {
+                    flag.push(k);
+                }
+            }
+        }
+        else if (minB == M[i][M[i].length - 1]) { //if min bound equal to augmented number, postive values clear and negative values mines
+            for (let k = 0; k < M[i].length - 1; k++) {
+                if (M[i][k] == -1 && flag.indexOf(k) === -1) {
+                    flag.push(k);
+                }
+                else if (M[i][k] == 1 && cleared.indexOf(k) === -1) {
+                    cleared.push(k);
+                }
+            }
+        }
+        
+        for (let k = 0; k < flag.length; k++) { //delete this later, but make sure that your logic isnt doo doo and you dont mark any cells as cleared and flagged
+            if (cleared.indexOf(flag[k]) != -1) {
+                console.log("ur stupid omg");
+            }
+        }
+        for (let k = 0; k < cleared.length; k++) { //delete this later, but make sure that your logic isnt doo doo and you dont mark any cells as cleared and flagged
+            if (flag.indexOf(cleared[k]) != -1) {
+                console.log("ur stupid omg");
+            }
+        }
+    }
+    return [cleared, flag];
+}
+
+function findPivot(M, col, nRow) {
+    let i_max = nRow;
+    for (let i = nRow + 1; i < M.length && Math.abs(M[i_max][col]) != 1; ++i) {
+        if (Math.abs(M[i][col]) == 1) {
+            i_max = i;
+        }
+    }
+    return i_max;
+}
+
+function swap_rows(M, nRow, i_max) {
+    if (i_max != nRow) {
+        let temp = M[i_max];
+        M[i_max] = M[nRow];
+        M[nRow] = temp;
+    }
+}
+
+function makeM(A, b) {
+    for (let i = 0; i < A.length; ++i) {
+        A[i].push(b[i]);
+    }
+}
+
+function solve(A, b) {
+    makeM(A, b);
+    diagonalize(A);
 }
 
 function getDiff() { //get dimensions based off radio box check in game settings.  the player is able to tick a different box than the game they are currently playing, but hopefully they don't do that lol
